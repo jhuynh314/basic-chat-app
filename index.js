96a35ea76c0ab64a3f0395fa407d826a362a4258
Jacob Huynh <jacobhuynh314@gmail.com>
2025-05-15 15:33:31 -0400

    Followed socket.io tutorial through step 8

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const { createServer } = require('node:http');
+const { join } = require('node:path');
+const { Server } = require('socket.io');
+const sqlite3 = require('sqlite3');
+const { open } = require('sqlite');
+
+async function main() {
+    //open the database
+    const db = await open({
+        filename: 'chat.db',
+        driver: sqlite3.Database
+    });
+
+    // create our 'messages' table (you cna ignore the 'client_offset' column for now)
+    await db.exec(`
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        client_offset TEXT UNIQUE,
+        content TEXT
+    );
+  `);
+
+    const app = express();
+    const server = createServer(app);
+    const io = new Server(server, {
+        connectionStateRecovery: {}
+    });
+
+    app.get('/', (req, res) => {
+        res.sendFile(join(__dirname, 'index.html'));
+    });
+
+    io.on('connection', async (socket) => {
+        console.log('a user connected');
+
+        socket.on('disconnect', () => {
+            console.log('user disconnected');
+        });
+
+        socket.on('chat message', async (msg, clientOffset, callback) => {
+            let result;
+            try {
+                // store the message in the database
+                result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?,?)', msg, clientOffset);
+            } catch (e) {
+                if (e.errno === 19 /*SQLITE_CONSTRAINT */) {
+                    // THE MESSAGE WAS ALREADY INSERTED, SO WE NOTIFY THE CLIENT
+                    callback();
+                } else {
+                    // nothing to do , just let the client retry
+                }
+                return;
+            }
+            console.log('message: ' + msg);
+            io.emit('chat message', msg, result.lastID);
+            callback();
+        });
+
+        console.log(socket.recovered);
+
+        if (!socket.recovered) {
+            // if the connection state recovery was not successful
+            try {
+                await db.each('SELECT id, content FROM messages WHERE id > ?',
+                    [socket.handshake.auth.serverOffset || 0],
+                    (_err, row) => {
+                        socket.emit('chat message', row.content, row.id);
+                    }
+                )
+            } catch (e) {
+                // something went wrong
+            }
+        } 
+    });
+
+    server.listen(3000, () => {
+        console.log('server running at http://localhost:3000');
+    });
+}
+
+main();
